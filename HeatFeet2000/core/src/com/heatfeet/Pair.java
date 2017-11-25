95b7a16b7bd5d06e222834f7b04e3874d0847ec4
Panula Tomi <panulat1@niksula.hut.fi>
2017-11-25 16:55:37 +0200

    sulamisviiva

---
+++ HeatFeet2000/core/src/com/heatfeet/Pair.java
@@ -0,0 +1,14 @@
+package com.heatfeet;
+
+/**
+ * Created by Tomi Panula on 25.11.2017.
+ */
+
+public class Pair {
+        int _1;
+        int _2;
+        Pair(int st, int nd){
+            this._1 = st;
+            this._2 = nd;
+        }
+}
--- HeatFeet2000/core/src/com/heatfeet/Player.java
+++ HeatFeet2000/core/src/com/heatfeet/Player.java
@@ -6,23 +6,46 @@ package com.heatfeet;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import java.util.ArrayList;
 
 public class Player {
-    float x, y;
-
+    float x, y, width, height;
+    ArrayList<Pair> melts = new ArrayList();
     Sprite sprite;
     Accelometer meter;
-
+    int rmv = 0;
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
+        this.width = 100;
+        this.height = 100;
         this.sprite = new Sprite(new Texture("player.png"));
     }
 
     void move() {
 
+        melts.add(new Pair((int) x,(int) y));
+        if(rmv == 100){
+            melts.remove(0);
+        }else{
+            rmv++;
+        }
+        x+= 1;
+        y+= 1;
     }
+    /*void melt() {
+        //if(melts.contains())
+        for(int i = 1; i < melts.size(); i++){
+            System.out.print("MOI");
+            //sr.rectLine(melts.get(i-1)._1, melts.get(i-1)._2, melts.get(i)._1, melts.get(i)._2, i/5);
+            sr.rectLine(20+i*2,20+i*2,200,200, i/5);
+        }
+
+    }*/
     void draw(SpriteBatch batch) {
+        this.sprite.setPosition(x,y);
         this.sprite.draw(batch);
     }
 
